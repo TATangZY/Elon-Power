5b8fdb84d3149dafb04e3213665f112c5b12738b
TATangZY <tangdachan@qq.com>
2023-03-03 12:25:58 -0800

    make it work with main branch

--- find_tweets.py
+++ find_tweets.py
@@ -1,36 +1,35 @@
-import pandas as pd
-import numpy as np
-from datetime import date,datetime
 from collections import defaultdict
 
+import pandas as pd
+import numpy as np
 
-# timestamp = '2012-07-12'
 
 # extract an year and set the lookup dataset path
-def extract_tweets(timestamp,df):
+def extract_tweets(timestamp, df):
     '''
     This function returns the tweets for a given yyyy-mm-dd
-    
+
     args:
     timestamp: a string of format 'yyyy-mm-dd'
     df: the dataframe of the entire cleaned data
-    
+
     returns:
     A list of tweets if there are any tweets in the dataset on the given day
     '''
+    timestamp = pd.to_datetime(timestamp).tz_localize(
+        'US/Central').strftime('%Y-%m-%d')
 
     dates = np.array(df['date'])
     tweets = np.array(df['tweet'])
 
-
     tweet_dict = defaultdict(list)
-    for i in range(len(dates)):
-        temp = dates[i].split(' ')[0]
-        tweet_dict[temp].append(tweets[i])
-    if(timestamp in tweet_dict.keys()):
+    for i, date in enumerate(dates):
+        # get the timestamp of the date
+        date_ts = date.to_period(freq='D').strftime('%Y-%m-%d')
+        tweet_dict[date_ts].append(tweets[i])
+
+    if (timestamp in tweet_dict.keys()):
         return tweet_dict[timestamp]
     else:
-        return "no tweets for this day"
-
-if __name__== "__main__":
-    print(extract_tweets(timestamp))
+        print("no tweets for this day")
+        return []
